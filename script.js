e01d1a50b798d3739f146c601d78ea3cf5955606
MegaEmme <mikmusco@gmail.com>
2025-03-07 20:30:35 +0100

    solved snack 2

--- script.js
+++ script.js
@@ -1,71 +1,131 @@
 console.log("Mike's snacks 6");
 
-//*********************************************
-//SNACK 1 
-// ********************************************
-
-
-// cretae array
-
 const lighterBikeElement = document.getElementById('lighterBike');
 
 console.log(lighterBikeElement);
 
-const bikes = [
+//*********************************************
+//SNACK 1 
+// ********************************************
+//
+// cretae array
+//
+// const bikes = [
+//     {
+//         nome : "Basso",
+//         peso : 10
+//     },
+//     {
+//         nome : "Bianchi",
+//         peso : 16
+//     },
+//     {
+//         nome : "Colnago",
+//         peso : 14
+//     },
+//     {
+//         nome : "Pinarello",
+//         peso : 9
+//     },
+//     {
+//         nome : "Cinelli",
+//         peso : 18
+//     },
+//     {
+//         nome : "Cipollini",
+//         peso : 7
+//     },
+//     {
+//         nome : "Olmo",
+//         peso : 11
+//     },
+//     {
+//         nome : "Wilier",
+//         peso : 8
+//     }
+// ];
+//
+// console.log(bikes);
+//
+// // create function
+//
+// function lighterBike (bike){
+//     let lighterbike = bike[0];
+//
+//     for(let i=1; i<bike.length;i++){
+//
+//         if(bike[i].peso < lighterbike.peso){
+//             lighterbike = bike[i];
+//         }
+//     }
+//     return lighterbike;
+// }
+//
+// // result
+//
+// const result = lighterBike(bikes);
+//
+// console.log(result); 
+//
+// lighterBikeElement.innerHTML= (`La bici più leggera è: ${result.nome}, con un peso di ${result.peso} kg.`);
+
+
+//*********************************************
+//SNACK 2
+// ********************************************
+
+// create array
+
+const footballteams = [
     {
-        nome : "Basso",
-        peso : 10
+        nome : "Roma",
+        punti : 0,
+        falli : 0
     },
     {
-        nome : "Bianchi",
-        peso : 16
+        nome : "Lazio",
+        punti : 0,
+        falli : 0
     },
     {
-        nome : "Colnago",
-        peso : 14
+        nome : "Inter",
+        punti : 0,
+        falli : 0
     },
     {
-        nome : "Pinarello",
-        peso : 9
-    },
-    {
-        nome : "Cinelli",
-        peso : 18
-    },
-    {
-        nome : "Cipollini",
-        peso : 7
-    },
-    {
-        nome : "Olmo",
-        peso : 11
-    },
-    {
-        nome : "Wilier",
-        peso : 8
+        nome : "Milan",
+        punti : 0,
+        falli : 0
     }
-];
+]
 
-console.log(bikes);
+console.log(footballteams);
 
-// create function
+// create function randomNumbers
 
-function lighterBike (bike){
-    let lighterbike = bike[0];
-
-    for(let i=1; i<bike.length;i++){
-
-        if(bike[i].peso < lighterbike.peso){
-            lighterbike = bike[i];
-        }
-    }
-    return lighterbike;
+function randomNumbers (min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// result
+// get random numbers
 
-const result = lighterBike(bikes);
+for(let i = 0; i<footballteams.length;i++){
+    footballteams[i].punti = randomNumbers(0,100);
+    footballteams[i].falli = randomNumbers(0,200);
+}
 
-console.log(result); 
+// create new array nomi e falli
 
-lighterBikeElement.innerHTML= (`La bici più leggera è: ${result.nome}, con un peso di ${result.peso} kg.`);
+const updatedFootbalTeams = [];
+
+// push nomi e falli updated
+
+for (let i=0;i<footballteams.length;i++){
+    updatedFootbalTeams.push({
+        nome :footballteams[i].nome,
+        falli :footballteams[i].falli
+    }
+    )
+}
+
+console.log(updatedFootbalTeams);
